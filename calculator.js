70a5b15acadb2ce3ddfbe5417d601266b0bfb0bc
roadkillYeenXwX <igm0wuwfi@mozmail.com>
2022-08-28 18:58:37 +1000

    change minus sign derection when displaying numbers on the calculator screen with rtl text direction enabled

--- calculator.js
+++ calculator.js
@@ -1,33 +1,31 @@
-// calculator object
-// Recieve a number value from user with numInput method and store it as an
-// array in numberInput property.
-// Use operator methods to convert user input array into a number 
-// and push it into numberArray that holds result value of the previous
-// calculation with the initial value set to null.
-// Use calculate() method inside operator methods with arithmetic operator
-// argument to calculate a new value.
-// Push calculation results into calculatorOutput array to display results,
-// the html page receives data only from this array.
-// Change boolean values to deactivate methods such as decimalInput() to
-// prevent user from inserting multiple symbols as well as to empty the input
-// and output arrays and allow for a new number input.
+/* calculator object
+  Recieve input from user with numInput method and store it as an array in
+numberInput property. Use operator methods to convert user input array into a
+number and push it to numberArray that holds result value of the previous
+calculation with the initial value set to null.
+  Use calculate() method inside operator methods with arithmetic operator
+argument to calculate a new value. Push calculation results into
+calculatorOutput array to display results. Change boolean values to deactivate
+methods such as decimalInput() to prevent user from inserting multiple symbols
+as well as to empty the input and output arrays and allow for a new umber input.
+*/
 
 const calculator = {
-  numberInput: [],
-  numberArray: [null],
-  calculatorOutput: [],
-  decimalPointInserted: false,
-  operated: false,
-  operatorUsed: '+',
+  numberInput: [],         //stores user input 
+  numberArray: [null],     //holds two numeric values for calculation
+  calculatorOutput: [],    //displays calculation results 
+  decimalPointInserted: false, //to disable inserting multiple decimals
+  calculated: false,       //to turn off operator buttons after each calculation
+  operatorUsed: '+',       //passed as an argumant to calculate() method
 
   numInput(num) {
-    if (this.operated === true) {
+    if (this.calculated === true) {//clear ooutput array after every calculation
       this.calculatorOutput.length = 0;
-      this.operated = false;
-    }
-    if (this.decimalPointInserted === false && this.numberInput[0] == 0) {
-      this.numberInput.pop();
-      this.calculatorOutput.pop();
+      this.calculated = false;
+    }   //prevent entering multiple zeroes before other numbers or decimal point
+    if (this.decimalPointInserted === false && this.numberInput[0] === 0) {
+      this.numberInput.shift();
+      this.calculatorOutput.shift();
     
     } 
     this.numberInput.push(num);
@@ -36,7 +34,7 @@ const calculator = {
   },
 
   add() {
-    if (this.operated === false) {
+    if (this.calculated === false) {
       this.numberArray.push(Number(this.numberInput.join('')));
       this.numberInput.length = 0;
       this.calculate(this.operatorUsed);
@@ -46,7 +44,7 @@ const calculator = {
   },
 
   subtract() {
-    if (this.operated === false) {
+    if (this.calculated === false) {
       this.numberArray.push(Number(this.numberInput.join('')));
       this.numberInput.length = 0;
       this.calculate(this.operatorUsed);
@@ -67,7 +65,7 @@ const calculator = {
     this.numberArray.push(resultNumber);
     this.calculatorOutput.length = 0;
     this.calculatorOutput.push(this.roundIfTooBig(resultNumber));
-    this.operated = true;
+    this.calculated = true;
     this.decimalPointInserted = false;
   },
 
@@ -78,8 +76,8 @@ const calculator = {
     this.variables();
   },
 
-  decimalInput() {
-    if (this.decimalPointInserted === false && this.operated === false) {
+  decimalInput() {     //push 0. if '.' button clicked when input array is empty
+    if (this.decimalPointInserted === false && this.calculated === false) {
       if (this.numberInput.length === 0) {
         this.numberInput.push('0');
         this.calculatorOutput.push('0');
@@ -96,14 +94,17 @@ const calculator = {
     this.numberArray = [null];
     this.calculatorOutput = [];
     this.decimalPointInserted = false;
-    this.operated = false;
+    this.calculated = false;
     this.operatorUsed = '+';
     this.variables();
   },
 
+  /* round/truncate calc results that won't fit the calculator screen
+  only use this for big numbers meant to be displayed to the user
+  in calculatorOutput array */
   roundIfTooBig(number) {
     if (number.toString().includes('e')) {
-      return number.toPrecision(2);
+      return number.toPrecision(1);
     }
     if (number % 1 === 0) {
       return number;
@@ -111,7 +112,7 @@ const calculator = {
       const numberString = number.toString();
       const wholeLength = numberString.indexOf('.') + 1;
       const decimalLength = numberString.length - wholeLength;
-      if (decimalLength === 16) {
+      if (decimalLength > 15) {
         return Number(number.toFixed(7));
       } else {
         return number;
@@ -119,24 +120,26 @@ const calculator = {
     }
   },
 
+  //track all variables with print statements
   variables() {
     console.clear();
     console.log(`user input: ${this.numberInput.join('')}`);
     console.log(`numbers array: ${this.numberArray}`);
     console.log(`output: ${this.calculatorOutput.join('')}`);
     console.log(`decimal inserted: ${this.decimalPointInserted}`);
-    console.log(`operated: ${this.operated}`);
+    console.log(`calculated: ${this.calculated}`);
     console.log(`operator used: ${this.operatorUsed}`);
   },
 };
 
-// Assign event listeners to all calculator object elements within
-// a separate function. Change text display direction temporarily
-// when inserting bidi symbols to display them on the right
-// side. Use a for loop to automatically assign num pad event listeners
-// by changing the last index of the num button id string. Clicking a
-// button displays the content of the calculatorOutput array in
-// addition to calling one of the calculator object methods.
+/* 
+  Assign event listeners to all calculator object elements in a separate
+function. Change text display direction temporarily when inserting bidi symbols
+to display them on the right side. Use a for loop to automatically assign num
+pad event listeners by changing the last index of the num button id string.
+  Clicking a button displays the content of the calculatorOutput array in
+ addition to calling one of the calculator object methods.
+*/
 
 (() => {
   const primaryDisplay = document.getElementById('displayPrimary');
@@ -161,17 +164,20 @@ const calculator = {
   button = document.getElementById('addButton');
   button.addEventListener('click', () => {
     calculator.add();
-    primaryDisplay.innerHTML = calculator.calculatorOutput.join('');
+    primaryDisplay.innerHTML = 
+      changeMinusSignSide(calculator.calculatorOutput.join(''));
   });
   button = document.getElementById('subtractButton');
   button.addEventListener('click', () => {
     calculator.subtract();
-    primaryDisplay.innerHTML = calculator.calculatorOutput.join('');
+    primaryDisplay.innerHTML = 
+      changeMinusSignSide(calculator.calculatorOutput.join(''));
   });
   button = document.getElementById('equalButton');
   button.addEventListener('click', () => {
     calculator.equal();
-    primaryDisplay.innerHTML = calculator.calculatorOutput.join('');
+    primaryDisplay.innerHTML = 
+      changeMinusSignSide(calculator.calculatorOutput.join(''));
   });
   button = document.getElementById('clearButton');
   button.addEventListener('click', () => {
@@ -179,3 +185,16 @@ const calculator = {
     primaryDisplay.innerHTML = 0;
   });
 })();
+
+/* change the side of the minus sign from left to right in a number string to
+display it properly with text direction css property set to 'right to left' */
+function changeMinusSignSide(numString) {
+  numString = numString.split('');
+  if (numString[0] === '-') {
+    numString.push(numString[0]);
+    numString.shift();
+    return numString.join('');
+  } else {
+    return numString.join('');
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,18 @@
+function changeMinusSignSide(numString) {
+  numString = numString.split('');
+  if (numString[0] === '-') {
+    numString.push(numString[0]);
+    numString.shift();
+    return numString.join('');
+  } else {
+    return numString.join('');
+  }
+}
+
+
+console.clear();
+const t0 = performance.now();
+changeMinusSignSide('1.0e-14');
+const t1 = performance.now();
+console.log((t1 - t0).toFixed(3) + 'ms');
+console.log(changeMinusSignSide('1.0e-14'));
